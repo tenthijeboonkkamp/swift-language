b7e9bc87420798a82a636f3abfc18aacd12dbc27
Coen ten Thije Boonkkamp <cjtenthije@icloud.com>
2020-06-20 20:10:29 +0200

    working cli for liquidatie, dutch en burgerlijkwetboek beter verdeeld.

---
+++ Sources/Languages/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Coen ten Thije Boonkkamp on 19/06/2020.
+//
+
+import Foundation
+
+public protocol HasLanguage {
+    var language:Languages.Language { get }
+}
---
+++ Sources/Languages/stringforlanguages.swift
@@ -0,0 +1,41 @@
+//
+//  stringforlanguages.swift
+//  Trust
+//
+//  Created by Coen ten Thije Boonkkamp on 11/06/2020.
+//  Copyright © 2020 LegalKit. All rights reserved.
+//
+
+import Foundation
+
+public func string(for language:Languages.Language, dutch:String? = nil, english:String? = nil)->String {
+    switch language {
+    case .english: return english ?? ""
+    case .dutch: return dutch ?? ""
+    default: fatalError()
+    }
+}
+
+public func string(_ string:String, if condition:@autoclosure ()->Bool)->String? { condition() ? string : nil }
+
+public func string(dutch:String? = nil, english:String? = nil)->(Language)->String {
+    return { language in
+        switch language {
+        case .english: return english ?? ""
+        case .dutch: return dutch ?? ""
+        default: fatalError()
+        }
+    }
+}
+
+
+public func string<A>(from type: A, keyPath: KeyPath<A, Language>, dutch:String? = nil, english:String? = nil)->String {
+    let language = type[keyPath: keyPath]
+    switch language {
+    case .english: return english ?? ""
+    case .dutch: return dutch ?? ""
+    default: fatalError()
+    }
+}
+
+
