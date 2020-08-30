5aa2716e4b5b942e001ba36c99800b0d9a76677b
Coen ten Thije Boonkkamp <cjtenthije@icloud.com>
2020-08-30 16:39:35 +0200

    need to save before removing Dutch remote

--- Sources/Languages/Protocol.HasLanguage.swift
+++ Sources/Languages/Protocol.HasLanguage.swift
@@ -10,3 +10,16 @@ import Foundation
 public protocol HasLanguage {
     var language:Languages.Language { get }
 }
+
+extension Language:HasLanguage {
+    public var language: Language { self }
+}
+
+extension Languages.Language {
+    public var locale:Locale {
+        switch self {
+        case .english: return .init(identifier: "US_us")
+        case .dutch: return .init(identifier: "NL_nl")
+        }
+    }
+}
