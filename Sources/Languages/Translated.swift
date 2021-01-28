28ada7fb823a26c08895066b656fdece3c9b6c97
Coen ten Thije Boonkkamp <cjtenthije@icloud.com>
2021-01-28 22:22:09 +0100

    save

--- Sources/Languages/Translated.swift
+++ Sources/Languages/Translated.swift
@@ -7,10 +7,121 @@
 
 import Foundation
 
+public struct SinglePlural<A> {
+    public let single:A
+    public let plural:A
+    
+    public enum Variant {
+        case single
+        case plural
+    }
+    
+    public func callAsFunction(with variant:Variant = .single)->A {
+        callAsFunction(variant: variant)
+    }
+    
+    public func callAsFunction(in variant:Variant = .single)->A {
+        callAsFunction(variant: variant)
+    }
+    
+    public func callAsFunction(for variant:Variant = .single)->A {
+        callAsFunction(variant: variant)
+    }
+    public func callAsFunction(_ variant:Variant = .single)->A {
+        callAsFunction(variant:variant)
+    }
+    
+    public func callAsFunction(variant:Variant = .single)->A {
+        switch variant {
+        case .single: return self.single
+        case .plural: return self.plural
+        }
+    }
+    
+    public func map<B>(_ transform:(A)->B)->SinglePlural<B> {
+        return SinglePlural<B>.init(single: transform(self.single), plural: transform(self.plural))
+    }
+    
+    public func flatMap<B>(_ transform:(A)->SinglePlural<B>)->SinglePlural<B> {
+        return SinglePlural<B>.init(single: transform(self.single).single, plural: transform(self.plural).plural)
+    }
+    
+    public init(
+        _ all: (Variant)->A
+    ){
+        self.single = all(.single)
+        self.plural = all(.plural)
+    }
+    
+    public init(
+        
+        single:A,
+        plural:A
+    ){
+        self.single = single
+        self.plural = plural
+    }
+}
+
+public extension SinglePlural where A == Translated<String> {
+    static let parties:Self = .init(single: .init(english: "Party", dutch: "Partij"), plural: .init(english: "Parties", dutch: "Partijen"))
+}
+
+extension SinglePlural:CustomStringConvertible where A == Translated<String> {
+    
+    public init(
+        _ string:Translated<String>
+    ) {
+        self.single = string
+        self.plural = string
+    }
+    
+    public init(
+        english:String,
+        dutch:String
+    ) {
+        self.single = .init(english: english, dutch: dutch)
+        self.plural = .init(english: english, dutch: dutch)
+    }
+    
+    public var description: String {
+        self.single.callAsFunction(language: .current)
+    }
+}
+
+extension SinglePlural: ExpressibleByUnicodeScalarLiteral where A == Translated<String> {
+    public init(unicodeScalarLiteral value: Translated<String>) {
+        self.single = value
+        self.plural = value
+    }
+    
+    public typealias UnicodeScalarLiteralType = String
+    
+}
+
+extension SinglePlural: ExpressibleByExtendedGraphemeClusterLiteral where A == Translated<String> {
+    public typealias ExtendedGraphemeClusterLiteralType = String
+    
+}
+
+extension SinglePlural:ExpressibleByStringLiteral where A == Translated<String> {
+    public init(stringLiteral:String) {
+        self.single = .init(stringLiteral)
+        self.plural = .init(stringLiteral)
+    }
+}
+
+public extension SinglePlural where A == Translated<String> {
+    static let empty:Self = .init(single: "", plural: "")
+}
+
+extension SinglePlural:Equatable where A:Equatable {}
+extension SinglePlural:Hashable where A:Hashable {}
+
 public struct Translated<A> {
     
-    public let english:A
-    public let dutch:A
+    public var english:A
+    public var dutch:A
     
     public func callAsFunction(with language:Languages.Language = .current)->A {
         callAsFunction(language: language)
