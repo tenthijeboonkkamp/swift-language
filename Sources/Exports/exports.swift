e76c7634a4b8b258ffff47634dec4e7d8904c0fa
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.com>
2024-07-19 16:29:23 +0200

    wip

--- Sources/Exports/exports.swift
+++ Sources/Exports/exports.swift
@@ -6,3 +6,10 @@
 //
 
 @_exported import Language
+@_exported import Language_Dependency
+@_exported import Locale
+@_exported import SinglePlural
+@_exported import String
+@_exported import Translated
+@_exported import TranslatedString
+
